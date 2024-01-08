ab8b70a6e3fa0280dc6709c1f8a2719bc5b763e5
valentin.aursulesei <valentin.aursulesei@fortech.ro>
2024-01-08 15:20:51 +0200

    Update

--- L0_Terraform/providers.tf
+++ L0_Terraform/providers.tf
@@ -7,11 +7,7 @@ terraform {
   }
 }
 
-
 provider "azurerm" {
+  skip_provider_registration = "true"
   features {}
-  use_msi = true
-  subscription_id = var.az_managed_identity_subscription_id
-  tenant_id = var.az_managed_identity_tenant_id
-  client_id = var.az_managed_identity_client_id
 }
