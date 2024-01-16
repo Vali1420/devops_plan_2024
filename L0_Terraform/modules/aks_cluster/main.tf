345eb2ab8dcb290e761dab0932431183ddf62719
valentin.aursulesei <valentin.aursulesei@fortech.ro>
2024-01-16 19:15:42 +0200

    Update

--- L0_Terraform/modules/aks_cluster/main.tf
+++ L0_Terraform/modules/aks_cluster/main.tf
@@ -1,9 +1,9 @@
-resource "azurerm_subnet" "default" {
-  name                 = "aks-cluster"
-  resource_group_name  = var.resource_group_name
-  address_prefixes     = ["10.0.4.0/22"]
-  virtual_network_name = var.vnet_name
-}
+# resource "azurerm_subnet" "default" {
+#   name                 = "aks-cluster"
+#   resource_group_name  = var.resource_group_name
+#   address_prefixes     = ["10.0.4.0/22"]
+#   virtual_network_name = var.vnet_name
+# }
 
 resource "azurerm_kubernetes_cluster" "example" {
   name                = "akscarwebapplication12"
@@ -17,6 +17,12 @@ resource "azurerm_kubernetes_cluster" "example" {
     vm_size    = "standard_b2s"
     vnet_subnet_id = azurerm_subnet.default.id
 
+  }
+
+  network_profile {
+    network_plugin = "kubenet"
+    pod_cidr = "10.1.0.0/16"
+    dns_service_ip = "10.1.0.10"
   }
 
   identity {
