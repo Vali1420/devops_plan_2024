7ff1e4d6be771756443121f8abf09247690bda5f
valentin.aursulesei <valentin.aursulesei@fortech.ro>
2024-02-02 15:44:22 +0200

    Update

--- L0_Terraform/main.tf
+++ L0_Terraform/main.tf
@@ -1,37 +1,37 @@
-# module "vnet"{
-#     source="./modules/vnet"
-#     vnet_name = var.vnet_name
-#     resource_group_location = var.resource_group_location
-#     resource_group_name = var.resource_group_name
-# }
+module "vnet"{
+    source="./modules/vnet"
+    vnet_name = var.vnet_name
+    resource_group_location = var.resource_group_location
+    resource_group_name = var.resource_group_name
+}
 
-# module "acr"{
-#     source="./modules/acr"
-#     resource_group_location = var.resource_group_location
-#     resource_group_name = var.resource_group_name
-# }
+module "acr"{
+    source="./modules/acr"
+    resource_group_location = var.resource_group_location
+    resource_group_name = var.resource_group_name
+}
 
-# module "mysql"{
-#     source="./modules/db_mysql"
-#     resource_group_location = var.resource_group_location
-#     resource_group_name = var.resource_group_name
-#     mysql_admin = var.mysql_admin
-#     mysql_password = var.mysql_password
-#     vnet_name = var.vnet_name
-#     azure_virtual_network_id = module.vnet.vnet_id
-#     depends_on=[module.vnet]
-# }
+module "mysql"{
+    source="./modules/db_mysql"
+    resource_group_location = var.resource_group_location
+    resource_group_name = var.resource_group_name
+    mysql_admin = var.mysql_admin
+    mysql_password = var.mysql_password
+    vnet_name = var.vnet_name
+    azure_virtual_network_id = module.vnet.vnet_id
+    depends_on=[module.vnet]
+}
 
-# module "postgres"{
-#     source="./modules/db_pg"
-#     resource_group_location = var.resource_group_location
-#     resource_group_name = var.resource_group_name
-#     postgres_admin = var.postgres_admin
-#     postgres_password = var.postgres_password
-#     vnet_name = var.vnet_name
-#     azure_virtual_network_id = module.vnet.vnet_id
-#     depends_on=[module.vnet]
-# }
+module "postgres"{
+    source="./modules/db_pg"
+    resource_group_location = var.resource_group_location
+    resource_group_name = var.resource_group_name
+    postgres_admin = var.postgres_admin
+    postgres_password = var.postgres_password
+    vnet_name = var.vnet_name
+    azure_virtual_network_id = module.vnet.vnet_id
+    depends_on=[module.vnet]
+}
 
 module "aks_cluster"{
     source="./modules/aks_cluster"
